72f82a065a7122c48ad1747b3bff5049e426b789
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:19:37 +0500

    Add files via upload

---
+++ qno6.js
@@ -0,0 +1,6 @@
+// // QNO:6
+// // Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// console.log("\tAqsa Raees")
+// console.log("\n you are very nice person")
+// console.log("\tkainat rasheed")
+// console.log("\n you are not very nice  person")
